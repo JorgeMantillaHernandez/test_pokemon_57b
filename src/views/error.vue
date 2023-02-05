2fdbdba2d52afa04bf811144461e2223a1f91b5f
unknown <jemantilla9@gmail.com>
2023-02-05 13:25:41 -0500

    Add 404 page

--- src/views/error.vue
+++ src/views/error.vue
@@ -2,7 +2,10 @@
   <v-container class="text-center" fluid="">
     <v-row>
       <v-col cols="12">
-        <h1>404</h1>
+        <h1 class="mt-10">404 page not found</h1>
+        <img class="image-404" src="@/assets/images/404.png" />
+        <br />
+        <v-btn variant="text" to="/"> return </v-btn>
       </v-col>
     </v-row>
   </v-container>
@@ -13,4 +16,9 @@ export default {
   name: "error-view",
 };
 </script>
-<style></style>
+<style>
+.image-404 {
+  width: 250px;
+  margin-left: 50px;
+}
+</style>
